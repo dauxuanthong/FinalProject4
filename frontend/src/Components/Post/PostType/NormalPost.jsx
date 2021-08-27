25058f879e90342f23e630457530f55d3db05d3f
thongdxgcd18684@fpt.edu.vn <SexyMeoww@DESKTOP-K1JGHBT>
2021-08-27 21:57:02 +0700

    done-info in normalpost

--- frontend/src/Components/Post/PostType/NormalPost.jsx
+++ frontend/src/Components/Post/PostType/NormalPost.jsx
@@ -14,6 +14,9 @@ import { AiOutlineTags } from "react-icons/ai";
 import { RiProductHuntLine } from "react-icons/ri";
 import { FiArchive } from "react-icons/fi";
 import { GiMoneyStack } from "react-icons/gi";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 function NormalPost(props) {
   //STATE
@@ -25,27 +28,27 @@ function NormalPost(props) {
     { value: "Kitchen tools", label: "Kitchen tools" },
     { value: "Technological", label: "Technological" },
     { value: "Repair tools", label: "Repair tools" },
-    { value: "AnotherType ", label: "Another type" },
+    { value: "AnotherType", label: "Another type" },
   ]);
 
   //USE-FORM
   const form = useForm({
     initialValues: {
       productName: "",
-      // productType: [],
-      // typeDetail: "",
-      // productQuantity: 0,
-      // cost: 1000,
+      productType: [],
+      typeDetail: "",
+      productQuantity: 1,
+      cost: 1000,
     },
     validationRules: {
       productName: (value) => value.trim().length >= 1 && value.trim().length <= 50,
-      // productType: (value) => value.trim().length > 0,
-      // typeDetail: (value) => value.trim().length >= 1,
-      // productQuantity: (value) => value >= 1,
-      // cost: (value) => value >= 1000,
-      // email: (value) => /^\S+@\S+$/.test(value),
-      // password: (value) => /^(?=.*[0-9])(?=.*[a-z]).{6,32}$/.test(value),
-      // confirmPassword: (value) => (value === pass ? true : false),
+      productType: (value) => value.length > 0,
+      typeDetail: (value) => {
+        if (form.values.productType.some((item) => item === "AnotherType")) {
+          return value.trim().length >= 1 ? true : false;
+        }
+        return true;
+      },
     },
   });
 
@@ -53,17 +56,7 @@ function NormalPost(props) {
   const productNameTip = (
     <Tooltip
       label="Use the exact name of the product to make it easy to find and sell"
-      position="top"
-      placement="end"
-      withArrow
-    >
-      <BsInfoCircle />
-    </Tooltip>
-  );
-  const productTypeTip = (
-    <Tooltip
-      label="If you can't find a tag that matches your product, select 'Another type' and fill in the 'Type detail' section below."
-      position="top"
+      position="bottom"
       placement="end"
       withArrow
     >
@@ -71,10 +64,23 @@ function NormalPost(props) {
     </Tooltip>
   );
 
+  //SLIDER
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 2,
+    slidesToScroll: 2,
+  };
+
   //EVENT
   const handleSubmit = async (value) => {
     const data = {
       productName: value.productName,
+      productType: value.productType,
+      typeDetail: value.productType.some((item) => item === "AnotherType") ? value.typeDetail : "",
+      productQuantity: value.productQuantity,
+      productPrice: value.cost,
     };
     console.log(data);
   };
@@ -83,7 +89,8 @@ function NormalPost(props) {
       <form onSubmit={form.onSubmit(handleSubmit)}>
         <div className="first-part-div">
           <div className="normal-post-info-area">
-            <div>
+            {/*Product name*/}
+            <div className="normal-post-info-item">
               <TextInput
                 required="true"
                 label="Product name"
@@ -97,62 +104,58 @@ function NormalPost(props) {
                 }}
               />
             </div>
-            <div>
-              <MultiSelect></MultiSelect>
-            </div>
-            {/* <div>
+            {/*Product type*/}
+            <div className="normal-post-info-item">
               <MultiSelect
                 required
                 label="Product type"
                 icon={<AiOutlineTags />}
-                rightSection={productTypeTip}
                 data={productType}
+                multiline="false"
+                clearButtonLabel="Clear selection"
+                clearable
+                searchable
+                transitionDuration={150}
+                transition="pop-top-left"
+                transitionTimingFunction="ease"
+                maxDropdownHeight={160}
                 placeholder="Select product tags"
                 error={form.errors.productType && "Please choose your product type"}
                 onChange={(event) => {
-                  form.setFieldValue("productType", event.currentTarget.value);
+                  form.setFieldValue("productType", event);
                 }}
                 onFocus={() => {
                   form.setFieldError("productType", false);
                 }}
               />
-            </div> */}
-            {/* <div>
+            </div>
+            {/*AnotherType*/}
+            <div className="normal-post-info-item">
               <TextInput
-                disabled={(form) => {
-                  form.productType.some((item) => {
-                    item === "AnotherType";
-                  });
-                }}
+                disabled={!form.values.productType.some((item) => item === "AnotherType")}
                 required="true"
                 label="Type detail"
                 icon={<AiOutlineTags />}
                 error={form.errors.typeDetail && "Please specify valid type detail"}
-                value={form.values.typeDetail}
                 onChange={(event) => form.setFieldValue("typeDetail", event.currentTarget.value)}
                 onFocus={() => {
                   form.setFieldError("typeDetail", false);
                 }}
               />
-            </div> */}
-            {/* <div>
+            </div>
+            <div className="normal-post-info-item">
               <NumberInput
                 required="true"
                 label="Product quantity available"
                 icon={<FiArchive />}
-                min={1000}
-                step={100000}
+                min={1}
+                step={1}
                 error={form.errors.productQuantity && "Enter product quantity available"}
-                onChange={(event) =>
-                  form.setFieldValue("productQuantity", event.currentTarget.value)
-                }
-                onFocus={() => {
-                  form.setFieldError("productQuantity", false);
-                }}
+                onChange={(event) => form.setFieldValue("productQuantity", event)}
               />
-            </div> */}
-            {/* <div>
-              <div>
+            </div>
+            <div className="currency-area">
+              <div className="currency-input">
                 <NumberInput
                   required="true"
                   label="Product value"
@@ -160,22 +163,33 @@ function NormalPost(props) {
                   icon={<GiMoneyStack />}
                   min={1000}
                   step={100000}
-                  error={form.errors.cost && "Please price your product"}
-                  onChange={(event) => form.setFieldValue("cost", event.currentTarget.value)}
-                  onFocus={() => {
-                    form.setFieldError("cost", false);
-                  }}
+                  onChange={(event) => form.setFieldValue("cost", event)}
                 />
               </div>
-              <div>
+              <div className="currency-unit">
                 <p>vnd</p>
               </div>
+            </div>
+          </div>
+          <div className="normal-post-img-area">
+            <img className="normal-post-main-img" src={mainImgUrl}></img>
+            <div className="normal-post-sub-img">
+              <Slider {...settings}>
+                <div>
+                  <p>1</p>
+                </div>
+                <div>
+                  <p>2</p>
+                </div>
+                <div>
+                  <p>3</p>
+                </div>
+              </Slider>
+            </div>
+            {/* <div>
+              <input type="file" />
             </div> */}
           </div>
-          {/* <div className="normal-post-img-area">
-            <img className="normal-post-main-img" src={mainImgUrl}></img>
-            <div className="normal-post-sub-img"></div>
-          </div> */}
         </div>
         <Button type="submit">Post</Button>
       </form>
